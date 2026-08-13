5164b2dfabf65c920f616e6bdb32d795f9772716
Cattafish <142777814+Cattafish@users.noreply.github.com>
2026-08-14 00:09:50 +0800

    Add QRScreen class for displaying QR code

---
+++ src/client/java/online/kbpf/dg_lab/client/screen/QRScreen.java
@@ -0,0 +1,47 @@
+package online.kbpf.dg_lab.client.screen;
+
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+@Environment(EnvType.CLIENT)
+public class QRScreen extends Screen {
+    private final Screen parent;
+    private final Identifier qrIdentifier;
+
+    public QRScreen(Screen parent, Identifier qrIdentifier) {
+        super(Text.literal("扫描二维码连接设备"));
+        this.parent = parent;
+        this.qrIdentifier = qrIdentifier;
+    }
+
+    @Override
+    protected void init() {
+        this.addDrawableChild(ButtonWidget.builder(Text.literal("返回"), button -> {
+            this.client.setScreen(this.parent);
+        }).dimensions(this.width / 2 - 100, this.height / 2 + 100, 200, 20).build());
+    }
+
+    @Override
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        super.render(context, mouseX, mouseY, delta);
+        
+        context.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, this.height / 2 - 110, 0xFFFFFF);
+        
+        if (this.qrIdentifier != null) {
+            // 在屏幕中央绘制 160x160 尺寸的二维码
+            context.drawTexture(this.qrIdentifier, this.width / 2 - 80, this.height / 2 - 90, 0.0F, 0.0F, 160, 160, 256, 256);
+        } else {
+            context.drawCenteredTextWithShadow(this.textRenderer, Text.literal("二维码生成失败"), this.width / 2, this.height / 2, 0xFF5555);
+        }
+    }
+
+    @Override
+    public void close() {
+        this.client.setScreen(this.parent);
+    }
+}
