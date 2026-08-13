160e852b9b1cd64d3e4f2969ccc4d0febda463a3
Cattafish <142777814+Cattafish@users.noreply.github.com>
2026-08-14 00:09:12 +0800

    Refactor CreateQR to return Identifier and use NativeImage
    Refactor CreateQR method to return Identifier and use NativeImage for QR code generation.

--- src/client/java/online/kbpf/dg_lab/client/createQR/ToolQR.java
+++ src/client/java/online/kbpf/dg_lab/client/createQR/ToolQR.java
@@ -4,6 +4,7 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import javax.imageio.ImageIO;
 
 import online.kbpf.dg_lab.client.Dg_labClient;
 import online.kbpf.dg_lab.client.Config.ModConfig;
@@ -13,16 +14,16 @@ import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.texture.NativeImageBackedTexture;
 import net.minecraft.text.Text;
-
-import javax.imageio.ImageIO;
-
+import net.minecraft.util.Identifier;
 
 public class ToolQR {
     private ToolQR() {
     }
 
-    public static void CreateQR() {
+    public static Identifier CreateQR() {
         ModConfig modConfig = Dg_labClient.modConfig;
         String ipAddress = modConfig.getAddress();
         if(ipAddress.equals("error")) {
@@ -30,38 +31,48 @@ public class ToolQR {
             if (client.player != null) {
                 client.player.sendMessage(Text.literal("没有指定的ip地址").withColor(0xFF5555), false);
             }
+            return null;
         }
-        else {
-            int port = modConfig.getPort();
-            StringBuilder url = new StringBuilder("https://www.dungeon-lab.com/app-download.php#DGLAB-SOCKET#ws://").append(ipAddress).append(':').append(port).append("/1234-123456789-12345-12345-01");
-            String filePath = "QR.png";
-            try {
-                Map<EncodeHintType, Object> hints = new HashMap<>();
-                hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
-                hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
 
-                BitMatrix bitMatrix = new MultiFormatWriter().encode(url.toString(), BarcodeFormat.QR_CODE, 300, 300, hints);
+        int port = modConfig.getPort();
+        StringBuilder url = new StringBuilder("https://www.dungeon-lab.com/app-download.php#DGLAB-SOCKET#ws://").append(ipAddress).append(':').append(port).append("/1234-123456789-12345-12345-01");
+        String filePath = "QR.png";
 
-                BufferedImage image = new BufferedImage(300, 300, BufferedImage.TYPE_INT_RGB);
-                for (int x = 0; x < 300; x++) {
-                    for (int y = 0; y < 300; y++) {
-                        image.setRGB(x, y, bitMatrix.get(x, y) ? 0xFF000000 : 0xFFFFFFFF);
-                    }
+        try {
+            Map<EncodeHintType, Object> hints = new HashMap<>();
+            hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+            hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+            hints.put(EncodeHintType.MARGIN, 1);
+
+            int size = 256;
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(url.toString(), BarcodeFormat.QR_CODE, size, size, hints);
+
+            BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
+            NativeImage nativeImage = new NativeImage(NativeImage.Format.RGBA, size, size, false);
+
+            for (int x = 0; x < size; x++) {
+                for (int y = 0; y < size; y++) {
+                    boolean bit = bitMatrix.get(x, y);
+                    int color = bit ? 0xFF000000 : 0xFFFFFFFF;
+                    image.setRGB(x, y, color);
+                    nativeImage.setColor(x, y, color);
                 }
-
-                File qrCodeFile = new File(filePath);
-                ImageIO.write(image, "png", qrCodeFile);
-
-
-                ProcessBuilder pb = new ProcessBuilder("cmd", "/c", "start", "", "\"" + qrCodeFile.getAbsolutePath() + "\"");
-                pb.start();
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
             }
+
+            // 仍旧写出 QR.png 文件到游戏本地目录（删除了 cmd 弹窗指令）
+            File qrCodeFile = new File(filePath);
+            ImageIO.write(image, "png", qrCodeFile);
+
+            // 注册为 Minecraft 动态纹理供游戏内 UI 使用
+            NativeImageBackedTexture texture = new NativeImageBackedTexture(nativeImage);
+            Identifier identifier = Identifier.of("dg_lab", "dynamic_qr_code");
+            MinecraftClient.getInstance().getTextureManager().registerTexture(identifier, texture);
+
+            return identifier;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
         }
-
-
     }
 }
